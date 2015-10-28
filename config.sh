a2465d5e7a100233f4dbbb766194689fa9ad44a0
Han Gyu Park <hgpark@ParkHanui-MacBook-Pro.local>
2015-10-28 15:24:28 +0900

    Modified .bashrc for Mac OS X, created config.sh which copies the content to the home directory

---
+++ config.sh
@@ -0,0 +1,16 @@
+# Copies Configurations
+
+cp .bashrc ~
+echo Copied .bashrc
+
+cp .bash_profile ~
+echo Copied .bash_profile
+
+sourch ~/.bashrc
+echo Sourced it
+
+cp .screenrc ~
+echo Copied .screenrc
+
+cp .vimrc ~
+echo Copied .vimrc
