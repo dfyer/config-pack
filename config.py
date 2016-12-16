ad895eecbba984af2ede2be12f6d9616764f8bd2
Park, Han Gyu <hgpark@kdb.snu.ac.kr>
2016-12-16 17:12:38 +0900

    [Pymode] Max Width to 120

--- config.py
+++ config.py
@@ -1,52 +1,52 @@
 import os
 
-if __name__ == '__main__':
-    path = "files/"
+path = "files/"
 
-    # HYPERTERM
+# HYPERTERM
+flag = ''
+while (flag is not 'y') and (flag is not 'n'):
+    flag = raw_input("Copy configs for hyperterm? (y/n): ")
+
+if flag is "y":
+    os.system("cp " + path + "hyperterm.js ~/.hyperterm.js")
+    print "Copying .hyperterm.j ...Complete"
+
+# BASH
+flag = ''
+while (flag is not 'y') and (flag is not 'n'):
+    flag = raw_input("Copy configs for bash? (y/n): ")
+
+if flag is "y":
     flag = ''
     while (flag is not 'y') and (flag is not 'n'):
-        flag = raw_input("Copy configs for hyperterm? (y/n): ")
+        flag = raw_input("Are you using Mac OS X? (y/n): ")
 
     if flag is "y":
-        os.system("cp " + path + "hyperterm.js ~/.hyperterm.js")
-        print "Copying .hyperterm.j ...Complete"
+        print "Mac OS X"
+        os.system("cp " + path + "bashrc.macosx ~/.bashrc")
+    elif flag == 'n':
+        print "LINUX"
+        os.system("cp " + path + "bashrc.linux ~/.bashrc")
+    print "Copying .bashrc ...Complete"
 
-    # BASH
-    flag = ''
-    while (flag is not 'y') and (flag is not 'n'):
-        flag = raw_input("Copy configs for bash? (y/n): ")
+    os.system("cp " + path + "bash_profile ~/.bash_profile")
+    print "Copying .bash_profile ...Complete"
 
-    if flag is "y":
-        flag = ''
-        while (flag is not 'y') and (flag is not 'n'):
-            flag = raw_input("Are you using Mac OS X? (y/n): ")
+# SCREEEN
+flag = ''
+while (flag is not 'y') and (flag is not 'n'):
+    flag = raw_input("Copy configs for screen? (y/n): ")
 
-        if flag is "y":
-            print "Mac OS X"
-            os.system("cp " + path + "bashrc.macosx ~/.bashrc")
-        elif flag == 'n':
-            print "LINUX"
-            os.system("cp " + path + "bashrc.linux ~/.bashrc")
-        print "Copying .bashrc ...Complete"
+if flag is "y":
+    os.system("cp " + path + "screenrc ~/.screenrc")
+    print "Copying .screenrc ...Complete"
 
-        os.system("cp " + path + "bash_profile ~/.bash_profile")
-        print "Copying .bash_profile ...Complete"
+# VIM
+flag = ''
+while (flag is not 'y') and (flag is not 'n'):
+    flag = raw_input("Copy configs for vim? (y/n): ")
 
-    # SCREEEN
-    flag = ''
-    while (flag is not 'y') and (flag is not 'n'):
-        flag = raw_input("Copy configs for screen? (y/n): ")
-
-    if flag is "y":
-        os.system("cp " + path + "screenrc ~/.screenrc")
-        print "Copying .screenrc ...Complete"
-
-    # VIM
-    flag = ''
-    while (flag is not 'y') and (flag is not 'n'):
-        flag = raw_input("Copy configs for vim? (y/n): ")
-
-    if flag is "y":
-        os.system("cp " + path + "vimrc ~/.vimrc")
-        print "Copying .vimrc ...Complete"
+if flag is "y":
+    os.system("cp " + path + "vimrc ~/.vimrc")
+    os.system("cp " + path + "pylint.rc ~/.pylint.rc")
+    print "Copying .vimrc ...Complete"
