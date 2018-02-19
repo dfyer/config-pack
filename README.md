0123c9dd26f818f312a04a119303fe2b426a36a5
Park, Han Gyu <hgpark@kdb.snu.ac.kr>
2018-02-19 09:30:19 +0000

    For me, Syntastic is better than Python-mode. I only found it out hard way.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Configuration Pack v0.9.1
 
-Python 2.7 required. If you are using Python version 3.0 or above, use py3config.py
-
+Python 2.7 required. If you are using Python version 3.0 or above, use py3config.py 
 1. Copy the configuration files
 (.bashrc, .bash__profile, .screenrc, .vimrc)
 
@@ -12,6 +11,13 @@ Python 2.7 required. If you are using Python version 3.0 or above, use py3config
 > $ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 3. Open Vim, run :PluginInstall
+
+4. For vim-gitgutter, run below
+
+> cd /tmp && git clone git://github.com/airblade/vim-gitgutter.git
+or
+> git clone git://github.com/airblade/vim-gitgutter.git
+> cp -r vim-gitgutter/* ~/.vim/
 
 ### Additional Configuration
 
--- config.py
+++ config.py
@@ -6,37 +6,38 @@ import os
 
 # Handler
 def ask(question):
-    while True:
-        answer = raw_input(question + " (y/n): ")
-        return True if answer.lower() == 'y' else False
+  return True
+  while True:
+    #answer = raw_input(question + " (y/n): ")
+    return True if answer.lower() == 'y' else False
 
 if __name__ == '__main__':
-    path = "files/"
+  path = "files/"
 
-    # BASH
-    if ask("Copy configs for bash?"):
-        bash_path = path
-        if ask("Are you using Mac OS X?"):
-            bash_path += "bashrc.macosx"
-        else:
-            bash_path += "bashrc.linux"
+  # BASH
+  if ask("Copy configs for bash?"):
+    bash_path = path
+    if ask("Are you using Mac OS X?"):
+      bash_path += "bashrc.macosx"
+    else:
+      bash_path += "bashrc.linux"
 
-        if os.path.exists('~/.bashrc'):
-            with open('~/.bashrc', 'at') as dest:
-                with open(bash_path, 'rt') as source:
-                    for line in source:
-                        dest.write(line)
-        else:
-            os.system("cp " + bash_path + " ~/.bashrc")
+      if os.path.exists('~/.bashrc'):
+        with open('~/.bashrc', 'at') as dest:
+          with open(bash_path, 'rt') as source:
+            for line in source:
+              dest.write(line)
+      else:
+        os.system("cp " + bash_path + " ~/.bashrc")
         print("...Complete")
 
-    # SCREEEN
-    if ask("Copy configs for screen?"):
-        os.system("cp " + path + "screenrc ~/.screenrc")
-        print("...Complete")
+  # SCREEEN
+  if ask("Copy configs for screen?"):
+    os.system("cp " + path + "screenrc ~/.screenrc")
+    print("...Complete")
 
-    # VIM
-    if ask("Copy configs for vim?"):
-        os.system("cp " + path + "vimrc ~/.vimrc")
-        os.system("cp " + path + "pylint.rc ~/.pylint.rc")
-        print("...Complete")
+  # VIM
+  if ask("Copy configs for vim?"):
+    os.system("cp " + path + "vimrc ~/.vimrc")
+    os.system("cp " + path + "pylint.rc ~/.pylint.rc")
+    print("...Complete")
