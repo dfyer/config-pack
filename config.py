6e557e25b52feef4bcf5ab465a82c01696a73158
hgpark <gominidive@hotmail.com>
2019-01-18 17:40:24 +0900

    Timestamp on history cmd + refactored config.py

--- config.py
+++ config.py
@@ -1,40 +1,64 @@
 #!/usr/bin/env python3
 
 import os
+import argparse
 
-# Handler
-def ask(question):
-  while True:
-    answer = input(question + " (y/n): ")
-    return True if answer.lower() is 'y' else False
+parser = argparse.ArgumentParser()
+parser.add_argument("-A", "--all", action="store_true",
+                    help="Install all .*rc files")
+parser.add_argument("-b", "--bash", action="store_true",
+                    help="Install .bashrc")
+parser.add_argument("-v", "--vim", action="store_true",
+                    help="Install .vimrc")
+parser.add_argument("-s", "--screen", action="store_true",
+                    help="Install .screenrc")
+parser.add_argument("-m", "--mac", action="store_true",
+                    help="Check if installing mac (only affects .bashrc path)")
+parser.add_argument("--backup", action="store_true", default=True,
+                    help="Create backup for existing .*rc files")
+args = parser.parse_args()
 
-if __name__ == '__main__':
-  path = "files/"
+SRC_BASE = "files/"
 
-  # BASH
-  if ask("Copy configs for bash?"):
-    bash_path = path
-    if ask("Are you using Mac OS X?"):
-      bash_path += "bashrc.macosx"
-    else:
-      bash_path += "bashrc.linux"
+def get_bashrc():
+  if args.mac:
+    return SRC_BASE + "bashrc.macosx"
+  else:
+    return SRC_BASE + "bashrc.linux"
 
-      if os.path.exists('~/.bashrc'):
-        with open('~/.bashrc', 'at') as dest:
-          with open(bash_path, 'rt') as source:
-            for line in source:
-              dest.write(line)
-      else:
-        os.system("cp " + bash_path + " ~/.bashrc")
-        print("...Complete")
+# Setup
+targets = {}
+if args.all:
+  targets[os.path.expanduser("~/.bashrc")] = get_bashrc()
+  targets[os.path.expanduser("~/.vimrc")] = SRC_BASE + "vimrc"
+  targets[os.path.expanduser("~/.screenrc")] = SRC_BASE + "screenrc"
+else:
+  if args.bash:
+    targets[os.path.expanduser("~/.bashrc")] = get_bashrc()
+  if args.vim:
+    targets[os.path.expanduser("~/.vimrc")] = SRC_BASE + "vimrc"
+  if args.screen:
+    targets[os.path.expanduser("~/.screenrc")] = SRC_BASE + "screenrc"
+print("Targets: ")
+print("\n".join(["\t" + x for x in targets.keys()]))
 
-  # SCREEEN
-  if ask("Copy configs for screen?"):
-    os.system("cp " + path + "screenrc ~/.screenrc")
-    print("...Complete")
+if targets:
+# Backup
+  if args.backup:
+    print("Backup: ")
+    for dst in targets:
+      if os.path.exists(dst):
+        backup_cmd = ["cp", dst, dst+".backup"]
+        backup_cmd = " ".join(backup_cmd)
+        print("\t" + backup_cmd, end='')
+        os.exec(backup_cmd)
+        print("\t..backup complete!")
 
-  # VIM
-  if ask("Copy configs for vim?"):
-    os.system("cp " + path + "vimrc ~/.vimrc")
-    os.system("cp " + path + "pylint.rc ~/.pylint.rc")
-    print("...Complete")
+# Copy
+  print("Install: ")
+  for dst in targets:
+    install_cmd = ["cp", targets[dst], dst]
+    install_cmd = " ".join(install_cmd)
+    print("\t" + install_cmd, end='')
+    os.exec(install_cmd)
+    print("\t..install complete!")
